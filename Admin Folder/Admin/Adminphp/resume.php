446648bc7c447fa45ce3d811370c8e640594c2b9
Maykel011 <montes.johnmichael01120@gmail.com>
2025-04-02 09:34:09 +0800

    resume update

--- Admin Folder/Admin/Adminphp/resume.php
+++ Admin Folder/Admin/Adminphp/resume.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CEDOC FIVERES</title>
-    <link rel="stylesheet" href="../../Css/resumes.css">
+    <link rel="stylesheet" href="../../Css/resumesuser.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 
@@ -63,8 +63,31 @@
 </aside>
 
 <div class="main-content">
+    <div class="table-container">
     <h1 class="main-title">Resume</h1>
+
+    <table>
+            <thead>
+                <tr>
+                    <th>Full Name</th>
+                    <th>Program Course</th>
+                    <th>School/University</th>
+                    <th>Address</th>
+                    <th>Contact Number</th>
+                    <th>Email Address</th>
+                    <th>OJT Hours</th>
+                    <th>Role Position</th>
+                    <th>Resume</th>
+                    <th>Action</th>
+                    </tr>
+            </thead>
+            <tbody id="resume">
+                <!-- Users will be loaded here via JavaScript -->
+            </tbody>
+        </table>
+    </div>
 </div>
+
 <script src="../../js/resume.js"></script>
 </body>
 </html>
