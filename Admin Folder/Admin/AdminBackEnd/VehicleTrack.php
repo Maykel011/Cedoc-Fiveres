ac8e7f50c97963e772bb7872b18d8d147539e86d
Brix030523 <biksterlooking@gmail.com>
2025-04-08 14:56:19 +0800

    commit

--- Admin Folder/Admin/AdminBackEnd/VehicleTrack.php
+++ Admin Folder/Admin/AdminBackEnd/VehicleTrack.php
@@ -4,7 +4,7 @@ session_start();
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "sjcdrrmo";
+$dbname = "cedoc_fiveres";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
