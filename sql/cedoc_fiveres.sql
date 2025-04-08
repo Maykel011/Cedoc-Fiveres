932b8505652f6f43acfa344579d6aa8b86224668
Maykel011 <montes.johnmichael01120@gmail.com>
2025-04-08 11:34:52 +0800

    Update cedoc_fiveres.sql

--- sql/cedoc_fiveres.sql
+++ sql/cedoc_fiveres.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 08, 2025 at 03:29 AM
+-- Generation Time: Apr 08, 2025 at 03:33 AM
 -- Server version: 8.4.3
 -- PHP Version: 8.3.16
 
@@ -133,6 +133,33 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `employee_no`, `first_name`, `last_name`, `position`, `role`, `email`, `password`, `pin_code`, `created_at`, `updated_at`) VALUES
 (8, 'MZ-5062', 'Arthur Nathan', 'Alcantara', 'Supervisor', 'Admin', 'nathantwotoy@gmail.com', '$2y$10$gyLSpDU.f8lgLefC7L6t.e36.jMaGxeSmIO6M4XRYkE2a6WknrHdi', '111222', '2025-04-02 07:50:28', '2025-04-03 07:13:13');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicle_runs`
+--
+
+CREATE TABLE `vehicle_runs` (
+  `id` int NOT NULL,
+  `vehicle_team` varchar(255) NOT NULL,
+  `case_type` varchar(255) NOT NULL,
+  `transport_officer` varchar(255) NOT NULL,
+  `emergency_responders` varchar(255) NOT NULL,
+  `location` text NOT NULL,
+  `dispatch_time` datetime NOT NULL,
+  `back_to_base_time` datetime NOT NULL,
+  `case_image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `vehicle_runs`
+--
+
+INSERT INTO `vehicle_runs` (`id`, `vehicle_team`, `case_type`, `transport_officer`, `emergency_responders`, `location`, `dispatch_time`, `back_to_base_time`, `case_image`) VALUES
+(1, 'Alpha', 'Medical Case', 'Danielito Bernardo', 'George Villanueva', 'San Juan Elementary School', '2025-03-11 12:18:00', '2025-03-11 13:20:00', 'uploads/Air quality.png'),
+(2, 'Alpha', 'Medical Case', 'Michael Montes', 'David Odvina', 'FilOil', '2025-03-20 15:00:00', '2025-03-22 15:00:00', 'uploads/preview.png'),
+(3, 'Charlie', 'Medical Standby', 'Brixter Luquing', 'Kian Visaya', 'san juan pinaglabanan', '2025-03-20 16:10:00', '2025-03-20 18:00:00', 'uploads/Screenshot 2025-04-03 205923.png');
+
 --
 -- Indexes for dumped tables
 --
@@ -166,6 +193,12 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `vehicle_runs`
+--
+ALTER TABLE `vehicle_runs`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -192,6 +225,12 @@ ALTER TABLE `media_folders`
 --
 ALTER TABLE `users`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `vehicle_runs`
+--
+ALTER TABLE `vehicle_runs`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
